0f7199a3c2642c65b1d6358758f28d644d3dd1cf
Dominic Orchard <dom.orchard@gmail.com>
2022-03-09 10:09:56 +0000

    redact some stuff for start

--- 2022/Lam/Syntax.hs
+++ 2022/Lam/Syntax.hs
@@ -4,10 +4,3 @@
 module Lam.Syntax where
 
 type Identifier = String
-
--- Abstract-syntax tree for LambdaCore
-data Expr where
-    Abs :: Identifier -> Expr -> Expr -- \x -> e  [λ x . e]
-    App :: Expr       -> Expr -> Expr -- e1 e2
-    Var :: Identifier         -> Expr -- x
-  deriving Show
